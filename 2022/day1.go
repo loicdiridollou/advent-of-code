083d1f64bc2f2e7b412c844414f2f62b094b9cfe
Loic Diridollou <loic.diridollou@gmail.com>
2022-12-11 09:47:55 +0100

    Adding first day of 2022

--- 2022/day1.go
+++ 2022/day1.go
@@ -1,7 +1,66 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func addArray(numbs []string) int {
+	result := 0
+	for _, numb := range numbs {
+		num, _ := strconv.Atoi(numb)
+		result += num
+	}
+	return result
+}
+
+func addArrayInt(numbs []int) int {
+	result := 0
+	for _, numb := range numbs {
+		result += numb
+	}
+	return result
+}
+
+func maxArray(numbs []int) int {
+	for j := 1; j < len(numbs); j++ {
+		if numbs[j] > numbs[0] {
+			numbs[0] = numbs[j]
+		}
+	}
+	return numbs[0]
+}
+
+// part1 function
+func part1() int {
+	dat, _ := os.ReadFile("./day1-input")
+	s := strings.Split(string(dat), "\n\n")
+	var res []int
+
+	for i := 0; i < len(s); i++ {
+		res = append(res, addArray(strings.Split(s[i], "\n")))
+	}
+	return maxArray(res)
+}
+
+// part2 function
+func part2() int {
+	dat, _ := os.ReadFile("./day1-input")
+	s := strings.Split(string(dat), "\n\n")
+	var res []int
+
+	for i := 0; i < len(s); i++ {
+		res = append(res, addArray(strings.Split(s[i], "\n")))
+	}
+	sort.Ints(res)
+	return addArrayInt(res[len(res)-3:])
+}
 
 func main() {
-  fmt.Println("DONE")
+	fmt.Println(part1())
+	fmt.Println(part2())
+	fmt.Println("DONE")
 }

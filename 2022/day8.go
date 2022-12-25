ae0fb3652840d0f8c3af76d450a27b306c1873a3
Loic Diridollou <loic.diridollou@gmail.com>
2022-12-25 19:06:50 +0100

    Formatting the codE

--- 2022/day8.go
+++ 2022/day8.go
@@ -13,23 +13,23 @@ func convInt(s string) int {
 }
 
 func MaxArray(array []int) int {
-    var max = array[0]
-    for _, value := range array {
-        if max < value {
-            max = value
-        }
-    }
-    return max
+	max := array[0]
+	for _, value := range array {
+		if max < value {
+			max = value
+		}
+	}
+	return max
 }
 
 func formatInput(s []string) [][]int {
-  tbl := make([][]int, len(s)-1)
+	tbl := make([][]int, len(s)-1)
 	for i := range tbl {
 		for j := range string(s[i]) {
 			tbl[i] = append(tbl[i], convInt(string(s[i][j])))
 		}
 	}
-  return tbl
+	return tbl
 }
 
 func extColumn(board [][]int, columnIndex int) []int {
@@ -41,20 +41,20 @@ func extColumn(board [][]int, columnIndex int) []int {
 }
 
 func isVisible(i int, j int, tbl [][]int) bool {
-  col := extColumn(tbl, j)
-  var res bool
-  if MaxArray(tbl[i][:j]) < tbl[i][j] {
-    res = true
-  } else if MaxArray(tbl[i][j+1:]) < tbl[i][j] {
-    res = true
-  } else if MaxArray(col[:i]) < tbl[i][j] {
-    res = true
-  } else if MaxArray(col[i+1:]) < tbl[i][j] {
-    res = true
-  } else {
-    res = false
-  }
-  return res
+	col := extColumn(tbl, j)
+	var res bool
+	if MaxArray(tbl[i][:j]) < tbl[i][j] {
+		res = true
+	} else if MaxArray(tbl[i][j+1:]) < tbl[i][j] {
+		res = true
+	} else if MaxArray(col[:i]) < tbl[i][j] {
+		res = true
+	} else if MaxArray(col[i+1:]) < tbl[i][j] {
+		res = true
+	} else {
+		res = false
+	}
+	return res
 }
 
 func Reverse(input []int) []int {
@@ -68,61 +68,61 @@ func Reverse(input []int) []int {
 }
 
 func computeScore(i int, j int, tbl [][]int) int {
-  scores := make([]int, 0)
-  col := extColumn(tbl, j)
-  t1 := tbl[i][:j]
-  t2 := tbl[i][j+1:]
-  t3 := col[:i]
-  t4 := col[i+1:]
-  tot := [4][]int{Reverse(t1), t2, Reverse(t3), t4}
-  for _, lt := range tot {
-    flag := false
-    score := 0
-    for _, k := range lt {
-      if !flag {
-        if k < tbl[i][j] {
-          score++
-        } else if k == tbl[i][j] {
-          score++
-          flag = true
-        } else {
-          score++
-          flag = true
-        }
-      }
-    }
-    scores = append(scores, score)
-  }
-  return scores[0] * scores[1] * scores[2] * scores[3]
+	scores := make([]int, 0)
+	col := extColumn(tbl, j)
+	t1 := tbl[i][:j]
+	t2 := tbl[i][j+1:]
+	t3 := col[:i]
+	t4 := col[i+1:]
+	tot := [4][]int{Reverse(t1), t2, Reverse(t3), t4}
+	for _, lt := range tot {
+		flag := false
+		score := 0
+		for _, k := range lt {
+			if !flag {
+				if k < tbl[i][j] {
+					score++
+				} else if k == tbl[i][j] {
+					score++
+					flag = true
+				} else {
+					score++
+					flag = true
+				}
+			}
+		}
+		scores = append(scores, score)
+	}
+	return scores[0] * scores[1] * scores[2] * scores[3]
 }
 
 func part1() int {
 	dat, _ := os.ReadFile("./day8-input")
 	s := strings.Split(string(dat), "\n")
-  tbl := formatInput(s)
-  totVisible := 2 * len(tbl) + 2 * len(tbl[0]) - 4
+	tbl := formatInput(s)
+	totVisible := 2*len(tbl) + 2*len(tbl[0]) - 4
 
-  for i := 1; i < len(tbl) - 1; i++ {
-    for j := 1; j < len(tbl[0]) - 1; j++ {
-      if isVisible(i, j, tbl) {
-        totVisible++
-      }
-    }
-  }
+	for i := 1; i < len(tbl)-1; i++ {
+		for j := 1; j < len(tbl[0])-1; j++ {
+			if isVisible(i, j, tbl) {
+				totVisible++
+			}
+		}
+	}
 	return totVisible
 }
 
 func part2() int {
 	dat, _ := os.ReadFile("./day8-input")
 	s := strings.Split(string(dat), "\n")
-  tbl := formatInput(s)
-   totVisible := make([]int, 0)
+	tbl := formatInput(s)
+	totVisible := make([]int, 0)
 
-  for i := 1; i < len(tbl) - 1; i++ {
-    for j := 1; j < len(tbl[0]) - 1; j++ {
-      totVisible = append(totVisible, computeScore(i, j, tbl))
-    }
-  }
+	for i := 1; i < len(tbl)-1; i++ {
+		for j := 1; j < len(tbl[0])-1; j++ {
+			totVisible = append(totVisible, computeScore(i, j, tbl))
+		}
+	}
 	return MaxArray(totVisible)
 }
 

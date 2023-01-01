861534884e9bb3695f6c9d3513d5d2a27913b0a5
Loic Diridollou <loic.diridollou@gmail.com>
2023-01-01 15:02:14 -0800

    Formatting part two for day 15

--- 2022/day15/part_two.go
+++ 2022/day15/part_two.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-  "os"
-  "strings"
+	"os"
+	"strings"
 )
 
 type Sensor struct {
-  x int
-  y int
-  d int
+	x int
+	y int
+	d int
 }
 
 func validPoint(x, y int, sensors []Sensor) bool {
-  for _, sn := range sensors {
-    dxy := Abs(x-sn.x) + Abs(y-sn.y)
-    if  dxy <= sn.d {
-      return false
-    }
-  }
-  return true
+	for _, sn := range sensors {
+		dxy := Abs(x-sn.x) + Abs(y-sn.y)
+		if dxy <= sn.d {
+			return false
+		}
+	}
+	return true
 }
 
 func part2() int {
-  dat, _ := os.ReadFile("./day15-input")
+	dat, _ := os.ReadFile("./day15-input")
 	input := string(dat)[:len(string(dat))-1]
 	s := strings.Split(input, "\n")
 
@@ -32,30 +32,30 @@ func part2() int {
 		readings = append(readings, [2][2]int{pts[0], pts[1]})
 	}
 
-  var sensors []Sensor
+	var sensors []Sensor
 	for _, r := range readings {
 		distance := Manhattan(r[0], r[1])
-    sensors = append(sensors, Sensor{r[0][0], r[0][1], distance})
-  }
+		sensors = append(sensors, Sensor{r[0][0], r[0][1], distance})
+	}
 
-  signs := [4][2]int{{-1, 1}, {-1, -1}, {1, -1}, {1, 1}}
-  found := false
+	signs := [4][2]int{{-1, 1}, {-1, -1}, {1, -1}, {1, 1}}
+	found := false
 
-  for _, sn := range sensors {
-    for dx := 0; dx < sn.d+2; dx++ {
-      dy := (sn.d+1)-dx
-      for _, sign := range signs {
-        x := sn.x + dx * sign[0]
-        y := sn.y + dy * sign[1]
-        if !(x >= 0 && x <= 4000000 && y >= 0 && y <= 4000000) {
-          continue
-        }
-        if validPoint(x, y, sensors) && !found {
-          found = true
-          return x*4000000 + y
-        }
-      }
-    }
-  }
-  return 0
+	for _, sn := range sensors {
+		for dx := 0; dx < sn.d+2; dx++ {
+			dy := (sn.d + 1) - dx
+			for _, sign := range signs {
+				x := sn.x + dx*sign[0]
+				y := sn.y + dy*sign[1]
+				if !(x >= 0 && x <= 4000000 && y >= 0 && y <= 4000000) {
+					continue
+				}
+				if validPoint(x, y, sensors) && !found {
+					found = true
+					return x*4000000 + y
+				}
+			}
+		}
+	}
+	return 0
 }

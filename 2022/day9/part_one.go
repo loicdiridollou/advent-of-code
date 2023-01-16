ab38c921e3355f6719f7ca4a63d9436ed7e0c180
Loic Diridollou <loic.diridollou@gmail.com>
2023-01-16 11:12:41 -0800

    Formating the code for part one of day 9

--- 2022/day9/part_one.go
+++ 2022/day9/part_one.go
@@ -37,77 +37,77 @@ func parseInput(input []string) []Move {
 }
 
 func (coord Coord) Hash() string {
-  return fmt.Sprint(coord.r) + "_" + fmt.Sprint(coord.c)
+	return fmt.Sprint(coord.r) + "_" + fmt.Sprint(coord.c)
 }
 
 func (coord *Coord) Move(dir string) {
-  switch dir {
-  case "U":
-    coord.r = coord.r - 1
-  case "D":
-    coord.r = coord.r + 1
-  case "L":
-    coord.c = coord.c - 1
-  case "R":
-    coord.c = coord.c + 1
-  }
+	switch dir {
+	case "U":
+		coord.r = coord.r - 1
+	case "D":
+		coord.r = coord.r + 1
+	case "L":
+		coord.c = coord.c - 1
+	case "R":
+		coord.c = coord.c + 1
+	}
 }
 
 func Abs(num int) int {
-  if num < 0 {
-    return -num
-  }
-  return num
+	if num < 0 {
+		return -num
+	}
+	return num
 }
 
 func (coord *Coord) Adjust(head Coord) {
-  dr, dc := coord.r - head.r, coord.c - head.c
-  if Abs(dr) <= 1 && Abs(dc) <= 1 {
-    return
-  } else if Abs(dr) == 2 && dc == 0 {
-    if dr > 0 {
-      coord.r--
-    } else {
-      coord.r++
-    }
-  } else if Abs(dc) == 2 && dr == 0 {
-   if dc < 0 {
-      coord.c++
-    } else {
-      coord.c--
-    }
-  } else {
-    if dr > 0 && dc > 0 {
-      coord.r--
-      coord.c--
-    } else if dr > 0 && dc < 0 {
-      coord.r--
-      coord.c++
-    } else if dr < 0 && dc > 0 {
-      coord.r++
-      coord.c--
-    } else if dr < 0 && dc < 0 {
-      coord.r++
-      coord.c++
-    }
-  }
+	dr, dc := coord.r-head.r, coord.c-head.c
+	if Abs(dr) <= 1 && Abs(dc) <= 1 {
+		return
+	} else if Abs(dr) == 2 && dc == 0 {
+		if dr > 0 {
+			coord.r--
+		} else {
+			coord.r++
+		}
+	} else if Abs(dc) == 2 && dr == 0 {
+		if dc < 0 {
+			coord.c++
+		} else {
+			coord.c--
+		}
+	} else {
+		if dr > 0 && dc > 0 {
+			coord.r--
+			coord.c--
+		} else if dr > 0 && dc < 0 {
+			coord.r--
+			coord.c++
+		} else if dr < 0 && dc > 0 {
+			coord.r++
+			coord.c--
+		} else if dr < 0 && dc < 0 {
+			coord.r++
+			coord.c++
+		}
+	}
 }
 
 func part1() int {
 	dat, _ := os.ReadFile("./day9-input")
 	input := strings.Split(string(dat), "\n")
 	moves := parseInput(input)
-  head := Coord{0, 0}
-  tail := Coord{0, 0}
-  tail_positions := map[string]bool{tail.Hash(): true}
+	head := Coord{0, 0}
+	tail := Coord{0, 0}
+	tail_positions := map[string]bool{tail.Hash(): true}
 
-  for _, move := range moves {
-    for ln := 0; ln < move.length; ln++ {
-      head.Move(move.dir)
-      tail.Adjust(head)
-      tail_positions[tail.Hash()] = true
-    }
-  }
+	for _, move := range moves {
+		for ln := 0; ln < move.length; ln++ {
+			head.Move(move.dir)
+			tail.Adjust(head)
+			tail_positions[tail.Hash()] = true
+		}
+	}
 
 	return len(tail_positions)
 }

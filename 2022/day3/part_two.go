95d0c56f58bbca22deb4b9351ec58322dc5b71b2
Loic Diridollou <loic.diridollou@gmail.com>
2023-02-01 18:23:28 -0800

    Adding part two for day 3 with refactor

---
+++ 2022/day3/part_two.go
@@ -0,0 +1,17 @@
+package main
+
+import "strings"
+
+
+func part2(input string) int {
+	s := strings.Split(input, "\n")
+	var p1, p2, p3 string
+	var score int
+	for i := 0; i < (len(s)-1)/3; i++ {
+		p1 = s[3*i]
+		p2 = s[3*i+1]
+		p3 = s[3*i+2]
+		score += charToScore(findSameItemTriple(p1, p2, p3))
+	}
+	return score
+}

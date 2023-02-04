52748eb175c53bad53a085dfd25c1f80ce2ae07a
Loic Diridollou <loic.diridollou@gmail.com>
2023-02-03 18:07:07 -0800

    Updating main file for day 1 to support testing

--- 2022/day1/main.go
+++ 2022/day1/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Day 1")
-	fmt.Println("Part 1 result:", part1())
-	fmt.Println("Part 2 result:", part2())
+	fmt.Println("Day 3")
+  var dat []byte
+  if len(os.Args) > 1 && os.Args[1] == "test" {
+    dat, _ = os.ReadFile("./day1-test-input")
+  } else {
+    dat, _ = os.ReadFile("./day1-input")
+  }
+	fmt.Println("Part 1 result:", part1(string(dat)))
+  fmt.Println("Part 2 result:", part2(string(dat)))
 }

51af50741d311bcfa9df508b43684cd0e5991bd5
Loic Diridollou <loic.diridollou@gmail.com>
2023-02-03 18:10:28 -0800

    Add support for testing

--- 2022/day2/main.go
+++ 2022/day2/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("Day 2")
-	fmt.Println("Part 1 result:", part1())
-	fmt.Println("Part 2 result:", part2())
+  var dat []byte
+  if len(os.Args) > 1 && os.Args[1] == "test" {
+    dat, _ = os.ReadFile("./day2-test-input")
+  } else {
+    dat, _ = os.ReadFile("./day2-input")
+  }
+	fmt.Println("Part 1 result:", part1(string(dat)))
+  fmt.Println("Part 2 result:", part2(string(dat)))
 }

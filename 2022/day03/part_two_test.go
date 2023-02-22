94a8d1f5cd390fa68b0f1a3201b3eed2bd650c9d
Loic Diridollou <loic.diridollou@gmail.com>
2023-02-21 18:51:48 -0800

    Adding tests for day 03

---
+++ 2022/day03/part_two_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func Test_part2(t *testing.T) {
+  var dat []byte
+  dat, _ = os.ReadFile("./day03.testinput")
+  actual := part2(string(dat))
+  expected := 70
+  
+  if actual != expected {
+    t.Errorf("part2() = %v but expects %v", actual, expected)
+  }
+}
+

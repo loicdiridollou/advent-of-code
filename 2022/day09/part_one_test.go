7bccf796d9fe1d4cfdbcf81605a91d23af188db5
Loic Diridollou <loic.diridollou@gmail.com>
2023-03-05 13:44:59 -0800

    Adding testing to part one and two for day 09

---
+++ 2022/day09/part_one_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func Test_part1(t *testing.T) {
+	var dat []byte
+	dat, _ = os.ReadFile("./day09.testinput")
+	actual := part1(string(dat))
+	expected := 13
+
+	if actual != expected {
+		t.Errorf("part1() = %v but expects %v", actual, expected)
+	}
+}

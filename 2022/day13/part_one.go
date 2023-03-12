bced2dddc91edf0df99071a2036d784d954aa13d
Loic Diridollou <loic.diridollou@gmail.com>
2023-03-12 14:53:14 -0700

    Refactoring code for day 13

--- 2022/day13/part_one.go
+++ 2022/day13/part_one.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"os"
 	"strconv"
 	"strings"
 )
@@ -19,51 +18,51 @@ func readTree(input string) tree {
 	var number string
 
 	for _, chr := range input {
-    switch chr {
-    case '[' :
-      tmpTree := tree{-1, []*tree{}, temp}
-      temp.elements = append(temp.elements, &tmpTree)
-      temp = &tmpTree
-    case ']':
-      if len(number) > 0 {
-        num, _ := strconv.Atoi(number)
-        temp.valueLeaf = num
-        number = ""
-      }
-      temp = temp.father
-    case ',':
-      if len(number) > 0 {
-        num, _ := strconv.Atoi(number)
-        temp.valueLeaf = num
-        number = ""
-      }
-      temp = temp.father
-      tmpTree := tree{-1, []*tree{}, temp}
-      temp.elements = append(temp.elements, &tmpTree)
-      temp = &tmpTree
-    default:
-      number += string(chr)
-    }	
+		switch chr {
+		case '[':
+			tmpTree := tree{-1, []*tree{}, temp}
+			temp.elements = append(temp.elements, &tmpTree)
+			temp = &tmpTree
+		case ']':
+			if len(number) > 0 {
+				num, _ := strconv.Atoi(number)
+				temp.valueLeaf = num
+				number = ""
+			}
+			temp = temp.father
+		case ',':
+			if len(number) > 0 {
+				num, _ := strconv.Atoi(number)
+				temp.valueLeaf = num
+				number = ""
+			}
+			temp = temp.father
+			tmpTree := tree{-1, []*tree{}, temp}
+			temp.elements = append(temp.elements, &tmpTree)
+			temp = &tmpTree
+		default:
+			number += string(chr)
+		}
 	}
 	return root
 }
 
 func checkOrder(t1 tree, t2 tree) int {
-  switch {
-  case len(t1.elements) == 0 && len(t2.elements) == 0:
+	switch {
+	case len(t1.elements) == 0 && len(t2.elements) == 0:
 		if t1.valueLeaf > t2.valueLeaf {
 			return -1
 		} else if t1.valueLeaf == t2.valueLeaf {
 			return 0
-		} 
-    return 1
-	
-  case t1.valueLeaf >= 0:
-    return checkOrder(tree{-1, []*tree{&t1}, nil}, t2)
-  case t2.valueLeaf >= 0:
+		}
+		return 1
+
+	case t1.valueLeaf >= 0:
+		return checkOrder(tree{-1, []*tree{&t1}, nil}, t2)
+	case t2.valueLeaf >= 0:
 		return checkOrder(t1, tree{-1, []*tree{&t2}, nil})
-  default:
-    var i int
+	default:
+		var i int
 		for i = 0; i < len(t1.elements) && i < len(t2.elements); i++ {
 			ordered := checkOrder(*t1.elements[i], *t2.elements[i])
 			if ordered != 0 {
@@ -79,16 +78,15 @@ func checkOrder(t1 tree, t2 tree) int {
 	return 0
 }
 
-func part1() int {
-	dat, _ := os.ReadFile("./day13-input")
-	s := strings.Split(string(dat), "\n\n")
-  var order_num int
-  for i, pair := range s {
-    pair_split := strings.Split(pair, "\n")
-    p1, p2 := readTree(pair_split[0]), readTree(pair_split[1])
-    if checkOrder(p1, p2) == 1 {
-      order_num += i + 1
-    }
-  }
-  return order_num
+func part1(input string) int {
+	s := strings.Split(input, "\n\n")
+	var order_num int
+	for i, pair := range s {
+		pair_split := strings.Split(pair, "\n")
+		p1, p2 := readTree(pair_split[0]), readTree(pair_split[1])
+		if checkOrder(p1, p2) == 1 {
+			order_num += i + 1
+		}
+	}
+	return order_num
 }
--- 2022/day13/part_two.go
+++ 2022/day13/part_two.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"os"
 	"sort"
 	"strings"
 )
 
-func part2() int {
-  dat, _ := os.ReadFile("./day13-input")
-	s := strings.Split(strings.Join(strings.Split(string(dat), "\n\n"), "\n"), "\n")
-  s = s[:len(s)-1]
-  var packages []tree
-  for _, st := range s {
-    packages = append(packages, readTree(st))
-  }
-  packages = append(packages, readTree("[[2]]"))
-  packages = append(packages, readTree("[[6]]"))
-  sort.Slice(packages, func(i, j int) bool {return checkOrder(packages[i], packages[j]) == 1})
-  decoder := 1
-  for i, p := range packages {
-    if checkOrder(p, readTree("[[2]]")) == 0 || checkOrder(p, readTree("[[6]]")) == 0 {
-      decoder *= i+1
-    }
-  }
+func part2(input string) int {
+	s := strings.Split(strings.Join(strings.Split(input, "\n\n"), "\n"), "\n")
+	s = s[:len(s)-1]
+	var packages []tree
+	for _, st := range s {
+		packages = append(packages, readTree(st))
+	}
+	packages = append(packages, readTree("[[2]]"))
+	packages = append(packages, readTree("[[6]]"))
+	sort.Slice(packages, func(i, j int) bool { return checkOrder(packages[i], packages[j]) == 1 })
+	decoder := 1
+	for i, p := range packages {
+		if checkOrder(p, readTree("[[2]]")) == 0 || checkOrder(p, readTree("[[6]]")) == 0 {
+			decoder *= i + 1
+		}
+	}
 	return decoder
 }

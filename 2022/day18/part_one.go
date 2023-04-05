59ceb63ea5b6b4526b231c7bdc62dcdc6d829400
Loic Diridollou <loic.diridollou@gmail.com>
2023-04-04 17:28:44 -0700

    Refactoring part one of day 18

--- 2022/day18/part_one.go
+++ 2022/day18/part_one.go
@@ -3,7 +3,6 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"os"
 	"strconv"
 	"strings"
 )
@@ -14,112 +13,111 @@ func convInt(s string) int {
 }
 
 type Point struct {
-  x, y, z int
+	x, y, z int
 }
 
 func parseLine(input string) []Point {
-  s := strings.Split(string(input), "\n")
-  var list []Point
+	s := strings.Split(string(input), "\n")
+	var list []Point
 
-  for _, el := range s {
-    if len(el) == 0 {
-      continue
-    }
-    coord := strings.Split(el, ",")
-    list = append(list, Point{convInt(coord[0]), convInt(coord[1]), convInt(coord[2])})
-  }
-  return list
+	for _, el := range s {
+		if len(el) == 0 {
+			continue
+		}
+		coord := strings.Split(el, ",")
+		list = append(list, Point{convInt(coord[0]), convInt(coord[1]), convInt(coord[2])})
+	}
+	return list
 }
 
 func hashVal(x, y, z int) string {
-  return fmt.Sprint(x) + "_" + fmt.Sprint(y) + "_" + fmt.Sprint(z)
+	return fmt.Sprint(x) + "_" + fmt.Sprint(y) + "_" + fmt.Sprint(z)
 }
 
 func DFS(point Point, points []Point, points_map map[string]Point, visited map[string]bool) (int, map[string]bool) {
-  faces := 0
-  x, y, z := point.x, point.y, point.z
-  act := hashVal(x, y, z)
-  up := hashVal(x, y, z+1)
-  dn := hashVal(x, y, z-1)
-  lf := hashVal(x-1, y, z)
-  rt := hashVal(x+1, y, z)
-  ft := hashVal(x, y+1, z)
-  re := hashVal(x, y-1, z)
+	faces := 0
+	x, y, z := point.x, point.y, point.z
+	act := hashVal(x, y, z)
+	up := hashVal(x, y, z+1)
+	dn := hashVal(x, y, z-1)
+	lf := hashVal(x-1, y, z)
+	rt := hashVal(x+1, y, z)
+	ft := hashVal(x, y+1, z)
+	re := hashVal(x, y-1, z)
 
-  visited[act] = true
-  var ok, vst bool
-  var fcs int
-  
-  if _, ok = points_map[up]; ok {
-    if _, vst = visited[up]; !vst {
-      fcs, visited = DFS(Point{x, y, z+1}, points, points_map, visited)
-      faces += fcs
-    }
-  } else {
-    faces++
-  }
-  if _, ok = points_map[dn]; ok {
-    if _, vst = visited[dn]; !vst {
-      fcs, visited = DFS(Point{x, y, z-1}, points, points_map, visited)
-      faces += fcs
-    }
-  } else {
-    faces++
-  }
-  if _, ok = points_map[lf]; ok {
-    if _, vst = visited[lf]; !vst {
-      fcs, visited = DFS(Point{x-1, y, z}, points, points_map, visited)
-      faces += fcs
-    }
-  } else {
-    faces++
-  }
-  if _, ok = points_map[rt]; ok {
-    if _, vst = visited[rt]; !vst {
-      fcs, visited = DFS(Point{x+1, y, z}, points, points_map, visited)
-      faces += fcs
-    }
-  } else {
-    faces++
-  }
-  if _, ok = points_map[ft]; ok {
-    if _, vst = visited[ft]; !vst {
-      fcs, visited = DFS(Point{x, y+1, z}, points, points_map, visited)
-      faces += fcs
-    }
-  } else {
-    faces++
-  }
-  if _, ok = points_map[re]; ok {
-    if _, vst = visited[re]; !vst {
-      fcs, visited = DFS(Point{x, y-1, z}, points, points_map, visited)
-      faces += fcs
-    }
-  } else {
-    faces++
-  }
+	visited[act] = true
+	var ok, vst bool
+	var fcs int
 
-  return faces, visited
+	if _, ok = points_map[up]; ok {
+		if _, vst = visited[up]; !vst {
+			fcs, visited = DFS(Point{x, y, z + 1}, points, points_map, visited)
+			faces += fcs
+		}
+	} else {
+		faces++
+	}
+	if _, ok = points_map[dn]; ok {
+		if _, vst = visited[dn]; !vst {
+			fcs, visited = DFS(Point{x, y, z - 1}, points, points_map, visited)
+			faces += fcs
+		}
+	} else {
+		faces++
+	}
+	if _, ok = points_map[lf]; ok {
+		if _, vst = visited[lf]; !vst {
+			fcs, visited = DFS(Point{x - 1, y, z}, points, points_map, visited)
+			faces += fcs
+		}
+	} else {
+		faces++
+	}
+	if _, ok = points_map[rt]; ok {
+		if _, vst = visited[rt]; !vst {
+			fcs, visited = DFS(Point{x + 1, y, z}, points, points_map, visited)
+			faces += fcs
+		}
+	} else {
+		faces++
+	}
+	if _, ok = points_map[ft]; ok {
+		if _, vst = visited[ft]; !vst {
+			fcs, visited = DFS(Point{x, y + 1, z}, points, points_map, visited)
+			faces += fcs
+		}
+	} else {
+		faces++
+	}
+	if _, ok = points_map[re]; ok {
+		if _, vst = visited[re]; !vst {
+			fcs, visited = DFS(Point{x, y - 1, z}, points, points_map, visited)
+			faces += fcs
+		}
+	} else {
+		faces++
+	}
+
+	return faces, visited
 }
 
-func part1() int {
-	dat, _ := os.ReadFile("./day18-input")
-  points := parseLine(string(dat))
+func part1(input string) int {
+	points := parseLine(input)
 
-  points_map := make(map[string]Point, 0)
-  for _, point := range points {
-    points_map[hashVal(point.x, point.y, point.z)] = point
-  }
-  visited := make(map[string]bool, 0)
+	points_map := make(map[string]Point, 0)
+	for _, point := range points {
+		points_map[hashVal(point.x, point.y, point.z)] = point
+	}
+	visited := make(map[string]bool, 0)
 
-  faces := 0
-  var fcs int
-  for _, point := range points {
-    if _, vst := visited[hashVal(point.x, point.y, point.z)]; !vst {
-      fcs, visited = DFS(point, points, points_map, visited)
-      faces += fcs
-    }
-  }
+	faces := 0
+	var fcs int
+	for _, point := range points {
+		if _, vst := visited[hashVal(point.x, point.y, point.z)]; !vst {
+			fcs, visited = DFS(point, points, points_map, visited)
+			faces += fcs
+		}
+	}
 
-  return faces
+	return faces
 }

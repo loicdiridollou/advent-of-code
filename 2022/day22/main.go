7a77239e9ccc48a21b177f597278f6a86f7eee29
Loic Diridollou <loic.diridollou@gmail.com>
2023-04-05 20:09:21 -0700

    Refactoring main function for day 22

--- 2022/day22/main.go
+++ 2022/day22/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Day 21")
-	//fmt.Println("Part 1 result:", part1())
-	fmt.Println("Part 2 result:", part2())
+	fmt.Println("Day 22")
+	var dat []byte
+	dat, _ = os.ReadFile("./day22.input")
+
+	fmt.Println("Part 1 result:", part1(string(dat)))
+	fmt.Println("Part 2 result:", part2(string(dat)))
 }

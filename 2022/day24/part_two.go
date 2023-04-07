6c6bff7a4d47d86e7bbfe8b0e4d8caea0b60e030
Loic Diridollou <loic.diridollou@gmail.com>
2023-04-07 11:35:06 -0700

    Refactoring code for part two of day 24

--- 2022/day24/part_two.go
+++ 2022/day24/part_two.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"os"
 	"strings"
 )
 
-func part2() int {
-	dat, _ := os.ReadFile("./day24-input")
-	input := strings.Split(string(dat), "\n")
-	input = input[:len(input)-1]
+func part2(input string) int {
+	input_data := strings.Split(input, "\n")
+	input_data = input_data[:len(input_data)-1]
 
-	blizs, start, end := parseInput(input)
+	blizs, start, end := parseInput(input_data)
 	exp := Exp{start[0], start[1]}
-	limitb, limitr := findDim(input)
+	limitb, limitr := findDim(input_data)
 	round := 0
 	queue := []Exp{exp}
 	arrived := false

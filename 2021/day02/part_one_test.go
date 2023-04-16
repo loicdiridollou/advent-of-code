46d6f06658d6d5d545db88710191ed21d7c226a0
Loic Diridollou <loic.diridollou@gmail.com>
2023-04-16 13:50:04 -0700

    Adding test coverage to part one of day 02 of 2021

---
+++ 2021/day02/part_one_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func Test_part1(t *testing.T) {
+	var dat []byte
+	dat, _ = os.ReadFile("./day02.testinput")
+	actual := part1(string(dat))
+	expected := 150
+
+	if actual != expected {
+		t.Fatal("Error")
+	}
+}

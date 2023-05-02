7537f365bbeae46d3144a62432fe10541c7dadf2
Loic Diridollou <loic.diridollou@gmail.com>
2023-05-02 07:25:11 -0700

    Adding new main function for day 12 of 2021

--- 2021/day12/main.go
+++ 2021/day12/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Println("2021 - Day 12")
-	dat, _ := os.ReadFile("./day12.testinput")
+	dat, _ := os.ReadFile("./day12.input")
 	fmt.Println("Part 1 result:", part1(string(dat)))
 	fmt.Println("Part 2 result:", part2(string(dat)))
 }

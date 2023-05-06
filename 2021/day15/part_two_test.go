78decce3c11f1d9a55089dcbc4c03504b6e248fc
Loic Diridollou <loic.diridollou@gmail.com>
2023-05-05 21:34:06 -0700

    Adding test coverage to part two of day 15 of 2021

---
+++ 2021/day15/part_two_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func Test_part2(t *testing.T) {
+	var dat []byte
+	dat, _ = os.ReadFile("./day15.testinput")
+	actual := part2(string(dat))
+	expected := 315
+
+	if actual != expected {
+		t.Errorf("part2() = %v but expects %v", actual, expected)
+	}
+}

dfbe69944db31986000ce65c424e35ec52fc8807
Loic Diridollou <loic.diridollou@gmail.com>
2023-12-01 20:45:56 -0800

    Adding test coverage for 2023-01 part 2

---
+++ 2023/day01/part2.test.ts
@@ -0,0 +1,22 @@
+import { part2 } from "./part2";
+
+describe("Testing part 1 with a string", () => {
+  test("Value should be returned for a one-line string", () => {
+    expect(part2("onefw2fgjwl4jfowd5")).toBe(15);
+  });
+});
+
+describe("Testing part 1 with an empty string", () => {
+  test("Value 0 should be returned for an empty string", () => {
+    expect(part2("")).toBe(0);
+  });
+});
+
+describe("Testing part 1 with a multiple-line string", () => {
+  test("Value should be returned for a multiple-line string", () => {
+    expect(
+      part2(`gfonejrei3mbjgio3bjgr4
+    utioerwpq2nbjfudi4fjdthreeks`),
+    ).toBe(37);
+  });
+});

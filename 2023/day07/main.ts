16cc5f42c0c974ab0b1bdb64a63eee8c430a078f
Loic Diridollou <loic.diridollou@gmail.com>
2023-12-07 11:46:33 -0800

    Adding main function for day 7 of 2023

---
+++ 2023/day07/main.ts
@@ -0,0 +1,10 @@
+// Main file to run answers for day 06 of 2023
+
+import fs from "fs";
+import { part1 } from "./part1";
+import { part2 } from "./part2";
+
+const input = fs.readFileSync("./2023/day07/part1.txt", "utf-8");
+
+console.log("Part 1 result:", part1(input));
+console.log("Part 2 result:", part2(input));

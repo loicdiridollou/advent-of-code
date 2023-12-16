602d1def8770b0fbe02de179ecb712942c83fe29
Loic Diridollou <loic.diridollou@gmail.com>
2023-12-16 15:52:47 -0800

    Fixing tests for day 16 of 2023

--- 2023/day16/part1.test.ts
+++ 2023/day16/part1.test.ts
@@ -1,10 +1,18 @@
 import { part1 } from "./part1";
-import fs from "fs";
-
-const input = fs.readFileSync("./2023/day16/part1.test.txt", "utf-8");
 
 describe("Testing part 1 with a multiple-line string", () => {
   test("Value should be returned for a multiple-line string", () => {
-    expect(part1(input)).toBe(46);
+    expect(
+      part1(`.|...\\....
+|.-.\\.....
+.....|-...
+........|.
+..........
+.........\\
+..../.\\\\..
+.-.-/..|..
+.|....-|.\\
+..//.|....`),
+    ).toBe(46);
   });
 });
--- 2023/day16/part2.test.ts
+++ 2023/day16/part2.test.ts
@@ -1,9 +1,18 @@
-import fs from "fs";
 import { part2 } from "./part2";
 
-const input = fs.readFileSync("./2023/day16/part1.test.txt", "utf-8");
 describe("Testing part 2 with a multiple-line string", () => {
   test("Value should be returned for a multiple-line string", () => {
-    expect(part2(input)).toBe(51);
+    expect(
+      part2(`.|...\\....
+|.-.\\.....
+.....|-...
+........|.
+..........
+.........\\
+..../.\\\\..
+.-.-/..|..
+.|....-|.\\
+..//.|....`),
+    ).toBe(51);
   });
 });

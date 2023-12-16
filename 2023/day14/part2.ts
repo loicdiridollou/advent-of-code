a39e2657fa2ace3fc0f7866c3adae4d8319b8bc8
Loic Diridollou <loic.diridollou@gmail.com>
2023-12-15 17:16:32 -0800

    Improving part 2 of day 14 of 2023

--- 2023/day14/part2.ts
+++ 2023/day14/part2.ts
@@ -1,26 +1,9 @@
 // Part 2 for day 14 of 2023
 
-export function transpose(matrix: string[][]) {
+import { rollRocks } from "./part1";
+
+export default function transpose(matrix: string[][]) {
   return matrix[0].map((_, i) => matrix.map((row) => row[i]));
-}
-
-export function rollRocks(groups: string[][]): string[][] {
-  let newGroups = transpose(groups).map((group) => group.join(""));
-  newGroups = newGroups.map((group) =>
-    group
-      .split("#")
-      .map((group) =>
-        group
-          .split("")
-          .sort(function (x, _) {
-            return x == "O" ? -1 : 1;
-          })
-          .join(""),
-      )
-      .join("#"),
-  );
-
-  return transpose(newGroups.map((group) => group.split("")));
 }
 
 function rotate(matrix: string[][]): string[][] {

0bd6b59c3ce2a8a73d138a4c169c8c8fa9013394
Loic Diridollou <loic.diridollou@gmail.com>
2023-12-24 12:09:26 -0800

    Fixing test for day 24 of 2023

--- 2023/day24/part1.test.ts
+++ 2023/day24/part1.test.ts
@@ -3,17 +3,11 @@ import { part1 } from "./part1";
 describe("Testing part 1 with a multiple-line string", () => {
   test("Value should be returned for a multiple-line string", () => {
     expect(
-      part1(`...........
-.....###.#.
-.###.##..#.
-..#.#...#..
-....#.#....
-.##..S####.
-.##..#...#.
-.......##..
-.##.#.####.
-.##..##.##.
-...........`),
-    ).toBe(42);
+      part1(`19, 13, 30 @ -2,  1, -2
+18, 19, 22 @ -1, -1, -2
+20, 25, 34 @ -2, -2, -4
+12, 31, 28 @ -1, -2, -1
+20, 19, 15 @  1, -5, -3`),
+    ).toBe(0);
   });
 });

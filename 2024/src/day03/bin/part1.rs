6f5efafc80ca0075c03074173a67c9e224e634f3
Loic Diridollou <loic.diridollou@gmail.com>
2024-12-03 07:57:00 -0500

    Adding part 1 for day 3 of 2024 (rust)

--- 2024/src/day03/bin/part1.rs
+++ 2024/src/day03/bin/part1.rs
@@ -1,7 +1,15 @@
 //! # Advent of Code - Day 3 - Part One
+use regex::Regex;
 
-pub fn part1() -> usize {
-    return 0;
+pub fn part1(_input: &str) -> usize {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let mut count = 0;
+    for (_, [n1, n2]) in re.captures_iter(_input).map(|c| c.extract()) {
+        count += n1.parse::<usize>().unwrap() * n2.parse::<usize>().unwrap();
+    }
+
+    return count;
 }
 
 #[cfg(test)]
@@ -10,6 +18,7 @@ mod day03 {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), 0);
+        let _input = include_str!("../testinput.txt");
+        assert_eq!(part1(_input), 161);
     }
 }
--- 2024/src/day03/bin/part2.rs
+++ 2024/src/day03/bin/part2.rs
@@ -1,6 +1,17 @@
 //! # Advent of Code - Day 3 - Part Two
 
-pub fn part2() -> usize {
+use regex::Regex;
+
+pub fn part2(_input: &str) -> usize {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let mut count = 0;
+    for (_, [n1, n2]) in re.captures_iter(_input).map(|c| c.extract()) {
+        count += n1.parse::<usize>().unwrap() * n2.parse::<usize>().unwrap();
+    }
+
+    println!("{}", count);
+
     return 0;
 }
 
@@ -10,6 +21,7 @@ mod day03 {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), 0);
+        let _input = include_str!("../testinput.txt");
+        assert_eq!(part2(_input), 0);
     }
 }

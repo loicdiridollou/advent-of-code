66dcb36bdd4c2cb4ac05144dcaa54bf253feaed1
Loic Diridollou <loic.diridollou@gmail.com>
2024-12-04 18:31:48 -0500

    Add code for day 4 of 2024 (rust)

--- 2024/src/day04/bin/part1.rs
+++ 2024/src/day04/bin/part1.rs
@@ -1,7 +1,56 @@
 //! # Advent of Code - Day 4 - Part One
 
-pub fn part1() -> usize {
-    return 0;
+use std::collections::HashMap;
+
+pub fn part1(_input: &str) -> usize {
+    let maze: Vec<Vec<char>> = _input
+        .split("\n")
+        .filter(|&x| !x.is_empty())
+        .map(|c| c.chars().collect::<Vec<char>>())
+        .collect();
+
+    let directions = HashMap::from([
+        ("N", (-1, 0)),
+        ("S", (1, 0)),
+        ("E", (0, 1)),
+        ("W", (0, -1)),
+        ("NW", (-1, -1)),
+        ("NE", (-1, 1)),
+        ("SW", (1, -1)),
+        ("SE", (1, 1)),
+    ]);
+
+    let mut count = 0;
+    for i in 0..maze.len() {
+        for j in 0..maze[0].len() {
+            if maze[i][j] == 'X' {
+                for (_, (dr, dc)) in &directions {
+                    if explore(&maze, i as i32, j as i32, *dr as i32, *dc as i32, "XMAS") {
+                        count += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    return count as usize;
+}
+
+fn explore(maze: &Vec<Vec<char>>, r: i32, c: i32, dr: i32, dc: i32, chr: &str) -> bool {
+    let expected = chr.chars().next().unwrap();
+    if maze[r as usize][c as usize] == expected && chr == "S" {
+        // we have found the sequence
+        return true;
+    }
+    if maze[r as usize][c as usize] != expected {
+        // wrong character, exit early
+        return false;
+    }
+    if r + dr < maze.len() as i32 && r + dr >= 0 && c + dc < maze[0].len() as i32 && c + dc >= 0 {
+        return explore(maze, r + dr, c + dc, dr, dc, &chr[1..].to_string());
+    }
+
+    return false;
 }
 
 #[cfg(test)]
@@ -10,6 +59,7 @@ mod day04 {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), 0);
+        let _input = include_str!("../testinput.txt");
+        assert_eq!(part1(_input), 18);
     }
 }
--- 2024/src/day04/bin/part2.rs
+++ 2024/src/day04/bin/part2.rs
@@ -1,7 +1,58 @@
 //! # Advent of Code - Day 4 - Part Two
 
-pub fn part2() -> usize {
-    return 0;
+use std::usize;
+
+pub fn part2(_input: &str) -> usize {
+    let maze: Vec<Vec<char>> = _input
+        .split("\n")
+        .filter(|&x| !x.is_empty())
+        .map(|c| c.chars().collect::<Vec<char>>())
+        .collect();
+
+    let mut _count = 0;
+    for i in 1..(maze.len() - 1) {
+        for j in 1..(maze[0].len() - 1) {
+            if maze[i][j] == 'A' {
+                if validate_pattern(&maze, i as i32, j as i32) {
+                    _count += 1;
+                }
+            }
+        }
+    }
+
+    return _count as usize;
+}
+
+fn validate_pattern(maze: &Vec<Vec<char>>, r: i32, c: i32) -> bool {
+    if maze[(r - 1) as usize][(c - 1) as usize] == 'M'
+        && maze[(r - 1) as usize][(c + 1) as usize] == 'S'
+        && maze[(r + 1) as usize][(c - 1) as usize] == 'M'
+        && maze[(r + 1) as usize][(c + 1) as usize] == 'S'
+    {
+        return true;
+    }
+    if maze[(r - 1) as usize][(c - 1) as usize] == 'M'
+        && maze[(r - 1) as usize][(c + 1) as usize] == 'M'
+        && maze[(r + 1) as usize][(c - 1) as usize] == 'S'
+        && maze[(r + 1) as usize][(c + 1) as usize] == 'S'
+    {
+        return true;
+    }
+    if maze[(r - 1) as usize][(c - 1) as usize] == 'S'
+        && maze[(r - 1) as usize][(c + 1) as usize] == 'M'
+        && maze[(r + 1) as usize][(c - 1) as usize] == 'S'
+        && maze[(r + 1) as usize][(c + 1) as usize] == 'M'
+    {
+        return true;
+    }
+    if maze[(r - 1) as usize][(c - 1) as usize] == 'S'
+        && maze[(r - 1) as usize][(c + 1) as usize] == 'S'
+        && maze[(r + 1) as usize][(c - 1) as usize] == 'M'
+        && maze[(r + 1) as usize][(c + 1) as usize] == 'M'
+    {
+        return true;
+    }
+    return false;
 }
 
 #[cfg(test)]
@@ -10,6 +61,7 @@ mod day04 {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), 0);
+        let _input = include_str!("../testinput.txt");
+        assert_eq!(part2(_input), 9);
     }
 }

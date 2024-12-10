05ce0213ad7f9f9e7625d2761f85b8f8cb660955
Loic Diridollou <loic.diridollou@gmail.com>
2024-12-10 18:24:09 -0500

    Add code for day 10 of 2024 (rust)

--- 2024/src/day10/bin/part1.rs
+++ 2024/src/day10/bin/part1.rs
@@ -1,7 +1,52 @@
 //! # Advent of Code - Day 10 - Part One
 
-pub fn part1() -> usize {
-    return 0;
+use std::collections::HashSet;
+
+fn explore_maze(maze: &Vec<Vec<u32>>, r: usize, c: usize) -> HashSet<Vec<usize>> {
+    let expected = maze[r][c] + 1;
+    let mut hash_set = HashSet::new();
+    if maze[r][c] == 9 {
+        hash_set.insert(vec![r, c]);
+        return hash_set;
+    }
+
+    if r > 0 && maze[r - 1][c] == expected {
+        hash_set.extend(explore_maze(maze, r - 1, c));
+    }
+    if r < maze.len() - 1 && maze[r + 1][c] == expected {
+        hash_set.extend(explore_maze(maze, r + 1, c));
+    }
+    if c > 0 && maze[r][c - 1] == expected {
+        hash_set.extend(explore_maze(maze, r, c - 1));
+    }
+    if c < maze[0].len() - 1 && maze[r][c + 1] == expected {
+        hash_set.extend(explore_maze(maze, r, c + 1));
+    }
+
+    return hash_set;
+}
+
+pub fn part1(_input: &str) -> usize {
+    let maze = _input
+        .split("\n")
+        .filter(|x| !x.is_empty())
+        .map(|item| {
+            item.chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .collect::<Vec<Vec<u32>>>();
+
+    let mut count = 0;
+
+    for r in 0..maze.len() {
+        for c in 0..maze[0].len() {
+            if maze[r][c] == 0 {
+                count += explore_maze(&maze, r, c).len();
+            }
+        }
+    }
+    return count;
 }
 
 #[cfg(test)]
@@ -10,6 +55,7 @@ mod day10 {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), 0);
+        let _input = include_str!("../testinput.txt");
+        assert_eq!(part1(_input), 36);
     }
 }
--- 2024/src/day10/bin/part2.rs
+++ 2024/src/day10/bin/part2.rs
@@ -1,7 +1,49 @@
 //! # Advent of Code - Day 10 - Part Two
 
-pub fn part2() -> usize {
-    return 0;
+fn explore_maze(maze: &Vec<Vec<u32>>, r: usize, c: usize) -> usize {
+    let expected = maze[r][c] + 1;
+    if maze[r][c] == 9 {
+        return 1;
+    }
+    let mut count = 0;
+
+    if r > 0 && maze[r - 1][c] == expected {
+        count += explore_maze(maze, r - 1, c);
+    }
+    if r < maze.len() - 1 && maze[r + 1][c] == expected {
+        count += explore_maze(maze, r + 1, c);
+    }
+    if c > 0 && maze[r][c - 1] == expected {
+        count += explore_maze(maze, r, c - 1);
+    }
+    if c < maze[0].len() - 1 && maze[r][c + 1] == expected {
+        count += explore_maze(maze, r, c + 1);
+    }
+
+    return count;
+}
+
+pub fn part2(_input: &str) -> usize {
+    let maze = _input
+        .split("\n")
+        .filter(|x| !x.is_empty())
+        .map(|item| {
+            item.chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .collect::<Vec<Vec<u32>>>();
+
+    let mut count = 0;
+
+    for r in 0..maze.len() {
+        for c in 0..maze[0].len() {
+            if maze[r][c] == 0 {
+                count += explore_maze(&maze, r, c);
+            }
+        }
+    }
+    return count;
 }
 
 #[cfg(test)]
@@ -10,6 +52,7 @@ mod day10 {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), 0);
+        let _input = include_str!("../testinput.txt");
+        assert_eq!(part2(_input), 0);
     }
 }

8d4a1112131affc0052417394f688ebe6b673477
Loic Diridollou <loic.diridollou@gmail.com>
2024-12-14 10:32:44 -0500

    Add code for day 14 of 2024 (rust)

--- 2024/src/day14/bin/part1.rs
+++ 2024/src/day14/bin/part1.rs
@@ -1,7 +1,65 @@
 //! # Advent of Code - Day 14 - Part One
 
-pub fn part1() -> usize {
-    return 0;
+use std::collections::HashMap;
+
+use regex::Regex;
+
+#[derive(Debug)]
+pub struct Robot {
+    pub r: i32,
+    pub c: i32,
+    dr: i32,
+    dc: i32,
+}
+
+impl Robot {
+    pub fn move_robot(&mut self, n: i32, nr: i32, nc: i32) {
+        self.r = (self.r + self.dr * n).rem_euclid(nr);
+        self.c = (self.c + self.dc * n).rem_euclid(nc);
+    }
+}
+
+pub fn parse_robot(robot_str: &str) -> Robot {
+    let robot_re = Regex::new(r"p\=(-?\d+),(-?\d+) v\=(-?\d+),(-?\d+)").unwrap();
+    let ext = robot_re.captures(&robot_str).unwrap();
+    return Robot {
+        r: ext[2].parse::<i32>().unwrap(),
+        c: ext[1].parse::<i32>().unwrap(),
+        dr: ext[4].parse::<i32>().unwrap(),
+        dc: ext[3].parse::<i32>().unwrap(),
+    };
+}
+
+pub fn part1(_input: &str, nr: i32, nc: i32) -> usize {
+    let mut robots = _input
+        .split("\n")
+        .filter(|x| !x.is_empty())
+        .map(|robot_str| parse_robot(robot_str))
+        .collect::<Vec<Robot>>();
+
+    for robot in robots.iter_mut() {
+        robot.move_robot(100, nr, nc);
+    }
+
+    let mut quadrants = HashMap::new();
+    for robot in robots.iter_mut() {
+        if robot.r < nr / 2 && robot.c < nc / 2 {
+            *quadrants.entry(0).or_insert(0) += 1;
+        } else if robot.r < nr / 2 && robot.c > nc / 2 {
+            *quadrants.entry(1).or_insert(0) += 1;
+        } else if robot.r > nr / 2 && robot.c > nc / 2 {
+            *quadrants.entry(2).or_insert(0) += 1;
+        } else if robot.r > nr / 2 && robot.c < nc / 2 {
+            *quadrants.entry(3).or_insert(0) += 1;
+        }
+    }
+
+    let mut total = 1;
+    for (_, v) in quadrants.iter() {
+        total *= v;
+    }
+
+    return total;
 }
 
 #[cfg(test)]
@@ -10,6 +68,7 @@ mod day14 {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), 0);
+        let _input = include_str!("../testinput.txt");
+        assert_eq!(part1(_input, 7, 11), 12);
     }
 }
--- 2024/src/day14/bin/part2.rs
+++ 2024/src/day14/bin/part2.rs
@@ -1,7 +1,46 @@
 //! # Advent of Code - Day 14 - Part Two
 
-pub fn part2() -> usize {
-    return 0;
+use std::collections::HashMap;
+
+use crate::part1::{parse_robot, Robot};
+
+pub fn part2(_input: &str, nr: i32, nc: i32) -> usize {
+    let mut robots = _input
+        .split("\n")
+        .filter(|x| !x.is_empty())
+        .map(|robot_str| parse_robot(robot_str))
+        .collect::<Vec<Robot>>();
+
+    let mut best_iter: i64 = -1;
+    let mut min_sf = nr * nc * nr * nc;
+    for i in 0..(nr * nc) {
+        for robot in robots.iter_mut() {
+            robot.move_robot(1, nr, nc);
+        }
+        let mut quadrants = HashMap::new();
+        for robot in robots.iter_mut() {
+            if robot.r < nr / 2 && robot.c < nc / 2 {
+                *quadrants.entry(0).or_insert(0) += 1;
+            } else if robot.r < nr / 2 && robot.c > nc / 2 {
+                *quadrants.entry(1).or_insert(0) += 1;
+            } else if robot.r > nr / 2 && robot.c > nc / 2 {
+                *quadrants.entry(2).or_insert(0) += 1;
+            } else if robot.r > nr / 2 && robot.c < nc / 2 {
+                *quadrants.entry(3).or_insert(0) += 1;
+            }
+        }
+        let mut total = 1;
+
+        for (_, v) in quadrants.iter() {
+            total *= v;
+        }
+        if total < min_sf {
+            min_sf = total;
+            best_iter = i as i64;
+        }
+    }
+
+    return best_iter as usize + 1;
 }
 
 #[cfg(test)]
@@ -10,6 +49,7 @@ mod day14 {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), 0);
+        let _input = include_str!("../testinput.txt");
+        assert_eq!(part2(_input, 7, 11), 7);
     }
 }

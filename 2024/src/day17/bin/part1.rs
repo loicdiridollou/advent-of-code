9d4ba03136dc5c13f15cb3c4ea1e32be3ae476c0
Loic Diridollou <loic.diridollou@gmail.com>
2024-12-17 18:58:36 -0500

    Add code for day 17 of 2024 (rust)

--- 2024/src/day17/bin/part1.rs
+++ 2024/src/day17/bin/part1.rs
@@ -1,7 +1,67 @@
 //! # Advent of Code - Day 17 - Part One
 
-pub fn part1() -> usize {
-    return 0;
+use regex::Regex;
+
+fn combo(operand: i64, a: i64, b: i64, c: i64) -> i64 {
+    match operand {
+        0 => 0,
+        1 => 1,
+        2 => 2,
+        3 => 3,
+        4 => a,
+        5 => b,
+        6 => c,
+        _ => panic!("Issue"),
+    }
+}
+
+pub fn run_program(mut a: i64, mut b: i64, mut c: i64, program: &Vec<i64>) -> Vec<i64> {
+    let mut outputs = vec![];
+    let mut i = 0;
+    while i < program.len() {
+        let opcode = program[i];
+        let operand = program[i + 1];
+
+        if opcode == 0 {
+            a = a >> combo(operand, a, b, c);
+        } else if opcode == 1 {
+            b = b ^ operand;
+        } else if opcode == 2 {
+            b = combo(operand, a, b, c) % 8;
+        } else if opcode == 3 {
+            if a != 0 {
+                i = operand as usize;
+                continue;
+            }
+        } else if opcode == 4 {
+            b = b ^ c;
+        } else if opcode == 5 {
+            outputs.push(combo(operand, a, b, c) % 8);
+        } else if opcode == 6 {
+            b = a >> combo(operand, a, b, c);
+        } else if opcode == 7 {
+            c = a >> combo(operand, a, b, c);
+        }
+        i += 2;
+    }
+    return outputs;
+}
+
+pub fn part1(_input: &str) -> String {
+    let re =
+        Regex::new(r"Register A: (\d+)\nRegister B: (\d+)\nRegister C: (\d+)\n\nProgram: (.+)")
+            .unwrap();
+    let matching = re.captures(_input).unwrap();
+    let a = matching[1].to_string().parse::<i64>().unwrap();
+    let b = matching[2].to_string().parse::<i64>().unwrap();
+    let c = matching[3].to_string().parse::<i64>().unwrap();
+    let program = matching[4]
+        .split(",")
+        .map(|c| c.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+
+    let outputs = run_program(a, b, c, &program);
+    return outputs.iter().map(|&id| id.to_string() + ",").collect();
 }
 
 #[cfg(test)]
@@ -10,6 +70,7 @@ mod day17 {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), 0);
+        let _input = include_str!("../testinput.txt");
+        assert_eq!(part1(_input), "4,6,3,5,6,3,5,2,1,0,");
     }
 }
--- 2024/src/day17/bin/part2.rs
+++ 2024/src/day17/bin/part2.rs
@@ -1,7 +1,38 @@
 //! # Advent of Code - Day 17 - Part Two
 
-pub fn part2() -> usize {
+use regex::Regex;
+
+use crate::part1::run_program;
+
+fn solver(program: &Vec<i64>, a: i64, b: i64, c: i64, prg_pos: usize) -> usize {
+    for i in 0..8 {
+        let first_digit_out = run_program(a * 8 + i, b, c, program)[0];
+        if first_digit_out == program[prg_pos] {
+            if prg_pos == 0 {
+                return (a * 8 + i) as usize;
+            }
+            let e = solver(program, a * 8 + i, b, c, prg_pos - 1);
+            if e != 0 {
+                return e as usize;
+            }
+        }
+    }
     return 0;
+}
+
+pub fn part2(_input: &str) -> usize {
+    let re =
+        Regex::new(r"Register A: (\d+)\nRegister B: (\d+)\nRegister C: (\d+)\n\nProgram: (.+)")
+            .unwrap();
+    let matching = re.captures(_input).unwrap();
+    let b = matching[2].to_string().parse::<i64>().unwrap();
+    let c = matching[3].to_string().parse::<i64>().unwrap();
+    let program = matching[4]
+        .split(",")
+        .map(|c| c.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+
+    return solver(&program, 0, b, c, program.len() - 1);
 }
 
 #[cfg(test)]
@@ -10,6 +41,7 @@ mod day17 {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), 0);
+        let _input = "Register A: 2024\nRegister B: 0\nRegister C: 0\n\nProgram: 0,3,5,4,3,0";
+        assert_eq!(part2(_input), 117440);
     }
 }

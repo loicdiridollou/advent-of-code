97c0193e826ab66b10004fd5ae37861038198fb1
Loic Diridollou <loic.diridollou@gmail.com>
2024-12-20 20:24:02 -0500

    Improve code for day 20 part 2

--- 2024/src/day20/bin/part2.rs
+++ 2024/src/day20/bin/part2.rs
@@ -1,38 +1,35 @@
 //! # Advent of Code - Day 20 - Part Two
 
-pub fn part2(_input: &str) -> i32 {
-    let path = read_path(_input);
-    let mut number_of_cheats = 0;
+fn distance(path: &Vec<(usize, usize)>, start: usize, end: usize) -> usize {
+    return path
+        .get(start)
+        .unwrap()
+        .0
+        .abs_diff(path.get(end).unwrap().0)
+        + path
+            .get(start)
+            .unwrap()
+            .1
+            .abs_diff(path.get(end).unwrap().1);
+}
 
-    for start in 0..path.len() {
-        for end in (start + 100)..path.len() {
-            let manhattan_distance = path
-                .get(start)
-                .unwrap()
-                .0
-                .abs_diff(path.get(end).unwrap().0)
-                + path
-                    .get(start)
-                    .unwrap()
-                    .1
-                    .abs_diff(path.get(end).unwrap().1);
-            if manhattan_distance <= 20 && manhattan_distance <= end - start - 100 {
-                number_of_cheats += 1;
-            }
-        }
-    }
-
-    number_of_cheats
+fn find_char(map: &Vec<Vec<char>>, value: char) -> (usize, usize) {
+    map.iter()
+        .enumerate()
+        .flat_map(|(y, line)| line.iter().enumerate().map(move |(x, c)| (x, y, c)))
+        .find(|c| *c.2 == value)
+        .map(|c| (c.0, c.1))
+        .unwrap()
 }
 
 pub fn read_path(input: &str) -> Vec<(usize, usize)> {
     let map = input
         .lines()
-        .map(|l| l.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+        .map(|l| l.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
     let start = find_char(&map, 'S');
     let end = find_char(&map, 'E');
-    let mut path = Vec::<_>::new();
+    let mut path = Vec::<(usize, usize)>::new();
     path.push(start);
     let mut current = start;
     'parse_map: loop {
@@ -52,16 +49,23 @@ pub fn read_path(input: &str) -> Vec<(usize, usize)> {
             }
         }
     }
-    path
+    return path;
 }
 
-fn find_char(map: &Vec<Vec<char>>, value: char) -> (usize, usize) {
-    map.iter()
-        .enumerate()
-        .flat_map(|(y, line)| line.iter().enumerate().map(move |(x, c)| (x, y, c)))
-        .find(|c| *c.2 == value)
-        .map(|c| (c.0, c.1))
-        .expect(&format!("Char [{}] missing", value))
+pub fn part2(_input: &str) -> i32 {
+    let path = read_path(_input);
+    let mut number_of_cheats = 0;
+
+    for start in 0..path.len() {
+        for end in (start + 100)..path.len() {
+            let manhattan_distance = distance(&path, start, end);
+            if manhattan_distance <= 20 && manhattan_distance <= end - start - 100 {
+                number_of_cheats += 1;
+            }
+        }
+    }
+
+    return number_of_cheats;
 }
 
 #[cfg(test)]

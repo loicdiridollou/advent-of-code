ca4ad7987d1b65431fe25cf8ceb9bba8bce284d8
Loic Diridollou <loic.diridollou@gmail.com>
2024-12-23 20:21:25 -0500

    Adding part 1 of day23 of 2024

--- 2024/src/day23/bin/part1.rs
+++ 2024/src/day23/bin/part1.rs
@@ -1,7 +1,38 @@
 //! # Advent of Code - Day 23 - Part One
 
+use std::collections::HashMap;
+use std::collections::HashSet;
+
 pub fn part1(_input: &str) -> usize {
-    return 0;
+    let mut graph = HashMap::new();
+    _input.split("\n").filter(|c| !c.is_empty()).for_each(|x| {
+        let val = x.split("-").collect::<Vec<&str>>();
+        graph.entry(val[0]).or_insert(HashSet::new()).insert(val[1]);
+        graph.entry(val[1]).or_insert(HashSet::new()).insert(val[0]);
+    });
+
+    let mut connected_sets = HashSet::new();
+
+    for (key, list_val) in graph.iter() {
+        let a = key;
+        for el in list_val.iter() {
+            let b = el;
+            for c in graph.get(el).unwrap() {
+                if !graph.get(c).unwrap().contains(a) {
+                    continue;
+                }
+                if a == b || b == c || a == c {
+                    continue;
+                }
+                if a[..1] == *"t" || b[..1] == *"t" || c[..1] == *"t" {
+                    let mut v = [a, b, c];
+                    v.sort();
+                    connected_sets.insert(v);
+                }
+            }
+        }
+    }
+    return connected_sets.len();
 }
 
 #[cfg(test)]
@@ -10,6 +41,7 @@ mod day23 {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(""), 0);
+        let _input = include_str!("../testinput.txt");
+        assert_eq!(part1(_input), 7);
     }
 }

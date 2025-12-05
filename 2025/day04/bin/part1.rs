14c7c957f4ada30b7128c653966945c66b7f5f84
Loïc Diridollou <loic.diridollou@gmail.com>
2025-12-04 21:22:12 -0500

    Performance improvements for day 4 of 2025 in rust

--- 2025/day04/bin/part1.rs
+++ 2025/day04/bin/part1.rs
@@ -1,59 +1,55 @@
 // Advent of Code 2025 - Day 04 part 1
 
+use std::collections::HashSet;
+
+#[derive(Eq, PartialEq, Debug, Hash)]
+pub struct Roll {
+    pub r: i32,
+    pub c: i32,
+}
+
 pub fn part1(_input: &str) -> usize {
     let v1 = _input
         .lines()
         .map(|c| c.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
 
+    let mut v2: HashSet<Roll> = HashSet::new();
+    for (i, line) in v1.iter().enumerate() {
+        for (j, item) in line.iter().enumerate() {
+            if *item == '@' {
+                v2.insert(Roll {
+                    r: i as i32,
+                    c: j as i32,
+                });
+            }
+        }
+    }
+
+    let directions = [
+        (0, 1),
+        (1, 0),
+        (1, 1),
+        (0, -1),
+        (-1, 0),
+        (-1, -1),
+        (1, -1),
+        (-1, 1),
+    ];
+
     let mut counter = 0;
-    for i in 0..v1.len() {
-        for j in 0..v1[0].len() {
-            if v1[i][j] != '@' {
-                continue;
+    for roll in v2.iter() {
+        let mut dir_check = 0;
+        for dir in directions.iter() {
+            if v2.contains(&Roll {
+                r: (roll.r + dir.0),
+                c: (roll.c + dir.1),
+            }) {
+                dir_check += 1;
             }
-
-            let mut n = true;
-            let mut nw = true;
-            let mut ne = true;
-            let mut w = true;
-            let mut sw = true;
-            let mut s = true;
-            let mut se = true;
-            let mut e = true;
-
-            if i > 0 && v1[i - 1][j] == '@' {
-                n = false;
-            }
-            if i > 0 && j > 0 && v1[i - 1][j - 1] == '@' {
-                nw = false;
-            }
-            if i > 0 && j < v1[0].len() - 1 && v1[i - 1][j + 1] == '@' {
-                ne = false;
-            }
-            if j > 0 && v1[i][j - 1] == '@' {
-                w = false;
-            }
-            if j < v1[0].len() - 1 && v1[i][j + 1] == '@' {
-                e = false;
-            }
-            if i < v1[0].len() - 1 && v1[i + 1][j] == '@' {
-                s = false;
-            }
-            if i < v1[0].len() - 1 && j > 0 && v1[i + 1][j - 1] == '@' {
-                sw = false;
-            }
-            if i < v1[0].len() - 1 && j < v1[0].len() - 1 && v1[i + 1][j + 1] == '@' {
-                se = false;
-            }
-
-            let v3: i32 = vec![n, nw, ne, w, e, sw, s, se]
-                .into_iter()
-                .map(|c| if c { 1 } else { 0 })
-                .sum();
-            if v3 > 4 {
-                counter += 1;
-            }
+        }
+        if dir_check < 4 {
+            counter += 1;
         }
     }
 
@@ -67,6 +63,6 @@ mod day01 {
     #[test]
     fn test_part1() {
         let _input = include_str!("../testinput.txt");
-        assert_eq!(part1(_input), 43);
+        assert_eq!(part1(_input), 13);
     }
 }
--- 2025/day04/bin/part2.rs
+++ 2025/day04/bin/part2.rs
@@ -1,64 +1,64 @@
 //! # Advent of Code - Day 1 - Part Two
 
+use std::collections::HashSet;
+
+use crate::part1::Roll;
+
 pub fn part2(_input: &str) -> usize {
-    let mut v1 = _input
+    let v1 = _input
         .lines()
         .map(|c| c.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
 
-    let mut counter = 1;
+    let mut v2: HashSet<Roll> = HashSet::new();
+    for (i, line) in v1.iter().enumerate() {
+        for (j, item) in line.iter().enumerate() {
+            if *item == '@' {
+                v2.insert(Roll {
+                    r: i as i32,
+                    c: j as i32,
+                });
+            }
+        }
+    }
+
+    let directions = [
+        (0, 1),
+        (1, 0),
+        (1, 1),
+        (0, -1),
+        (-1, 0),
+        (-1, -1),
+        (1, -1),
+        (-1, 1),
+    ];
+
     let mut removed = 0;
-    while counter > 0 {
-        counter = 0;
-        for i in 0..v1.len() {
-            for j in 0..v1[0].len() {
-                if v1[i][j] != '@' {
-                    continue;
+    loop {
+        let mut to_remove: Vec<Roll> = vec![];
+        for roll in v2.iter() {
+            let mut dir_check = 0;
+            for dir in directions.iter() {
+                if v2.contains(&Roll {
+                    r: (roll.r + dir.0),
+                    c: (roll.c + dir.1),
+                }) {
+                    dir_check += 1;
                 }
-
-                let mut n = true;
-                let mut nw = true;
-                let mut ne = true;
-                let mut w = true;
-                let mut sw = true;
-                let mut s = true;
-                let mut se = true;
-                let mut e = true;
-
-                if i > 0 && v1[i - 1][j] == '@' {
-                    n = false;
-                }
-                if i > 0 && j > 0 && v1[i - 1][j - 1] == '@' {
-                    nw = false;
-                }
-                if i > 0 && j < v1[0].len() - 1 && v1[i - 1][j + 1] == '@' {
-                    ne = false;
-                }
-                if j > 0 && v1[i][j - 1] == '@' {
-                    w = false;
-                }
-                if j < v1[0].len() - 1 && v1[i][j + 1] == '@' {
-                    e = false;
-                }
-                if i < v1[0].len() - 1 && v1[i + 1][j] == '@' {
-                    s = false;
-                }
-                if i < v1[0].len() - 1 && j > 0 && v1[i + 1][j - 1] == '@' {
-                    sw = false;
-                }
-                if i < v1[0].len() - 1 && j < v1[0].len() - 1 && v1[i + 1][j + 1] == '@' {
-                    se = false;
-                }
-
-                let v3: i32 = vec![n, nw, ne, w, e, sw, s, se]
-                    .into_iter()
-                    .map(|c| if c { 1 } else { 0 })
-                    .sum();
-                if v3 > 4 {
-                    v1[i][j] = '.';
-                    counter += 1;
-                    removed += 1;
-                }
+            }
+            if dir_check < 4 {
+                removed += 1;
+                to_remove.push(Roll {
+                    r: roll.r,
+                    c: roll.c,
+                });
+            }
+        }
+        if to_remove.is_empty() {
+            break;
+        } else {
+            for v3 in to_remove.iter() {
+                v2.remove(v3);
             }
         }
     }
@@ -73,6 +73,6 @@ mod day01 {
     #[test]
     fn test_part2() {
         let _input = include_str!("../testinput.txt");
-        assert_eq!(part2(_input), 3121910778619);
+        assert_eq!(part2(_input), 43);
     }
 }
